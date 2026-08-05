f383fbb8abd3c1033cf63617adf49182ccca79af
siddhartha verma <siddharthaverma6213@gmail.com>
2026-08-06 01:30:21 +0530

    chore: increment versionCode to 2 for Play Store release upload

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,7 +19,7 @@ android {
         minSdk = 24
         //noinspection OldTargetApi
         targetSdk = 35
-        versionCode = 1
+        versionCode = 2
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
